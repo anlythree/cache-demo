45b98445309b1aba70224a9bac01a6218e2256c3
wangli <15610462126@163.com>
2020-09-07 21:37:30 +0800

    缓存演示项目同步

---
+++ src/test/java/CacheTest.java
@@ -0,0 +1,28 @@
+import dao.UserDao;
+import domain.User;
+import org.apache.ibatis.cache.Cache;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+
+public class CacheTest {
+
+    private static Configuration configuration;
+
+    static {
+        // 初始化MyBatis
+        SqlSessionFactoryBuilder factoryBuilder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory build = factoryBuilder.build(CacheTest.class.getResourceAsStream("mybatis-config.xml"));
+        configuration = build.getConfiguration();
+    }
+
+    public static void main(String[] args) {
+        // 基于类名称、获取缓存
+        Cache cache = configuration.getCache(UserDao.class.getName());
+        User user = new User();
+        cache.putObject(666,user);
+        cache.getObject(666);
+    }
+}
